41169657f7e08211ae008143ebb6006e9aa51c50
Viktor Kifer <kifervictor@gmail.com>
2017-02-23 14:48:20 +0100

    Added learning rate optimizer and learning process stopper, categorical accuracy information updates

--- keras_helper.py
+++ keras_helper.py
@@ -1,12 +1,16 @@
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from system import mkdir
 
+from sklearn.model_selection import train_test_split as dataset_split
+
+def train_test_split(X, Y, split=0.33):
+    return dataset_split(X, Y, test_size=split)
 
 def model_saver(model_name):
     mkdir('outputs/models/' + model_name)
     return ModelCheckpoint('outputs/models/' + model_name + '/model_{epoch:02d}.hdf5',
                            monitor='val_loss',
-                           verbose=0,
+                           verbose=1,
                            save_best_only=False,
                            save_weights_only=False,
                            mode='auto',
@@ -14,5 +18,9 @@ def model_saver(model_name):
 
 
 def model_learning_optimizer():
-    return ReduceLROnPlateau(monitor='loss', factor=0.2,
-                             patience=3, min_lr=0.1)
+    return ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                             patience=2, min_lr=0.1, verbose=1)
+
+
+def learning_stopper():
+    return EarlyStopping(monitor='val_loss', min_delta=0.01, patience=5, verbose=1)
