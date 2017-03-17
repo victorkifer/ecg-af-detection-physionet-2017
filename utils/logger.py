cfa7ebf54be20f6e9bb6b8ae9c13e14efd615a37
Viktor Kifer <viktor.kifer.dev@gmail.com>
2017-03-17 16:56:41 +0100

    print time in logs

--- utils/logger.py
+++ utils/logger.py
@@ -5,20 +5,27 @@ from utils import system
 
 
 class Tee(object):
+    __message__ = ""
+
     def __init__(self, filename):
         self.terminal = sys.stdout
         self.logfile = open(filename, "w")
 
     def write(self, message):
-        self.terminal.write(message)
-        self.logfile.write(message)
+        self.__message__ += message
+        if '\n' in self.__message__:
+            current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+            self.__message__ = current_time + " - " + self.__message__
+            self.terminal.write(self.__message__)
+            self.logfile.write(self.__message__)
+            self.__message__ = ""
 
     def flush(self):
         self.terminal.flush()
         self.logfile.flush()
 
 
-def log_to_files():
+def log_to_files(prefix="log"):
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     system.mkdir('logs')
-    sys.stdout = Tee('logs/log_' + current_time + '.log')
+    sys.stdout = Tee('logs/' + prefix + '_' + current_time + '.log')
